ff530baf8d7e0e6830804369b3156cd32826eb8c
panboxiaosa <864633261@qq.com>
2015-04-18 16:41:39 +0800

    debug

--- app/src/main/java/com/androider/legacy/data/Converter.java
+++ app/src/main/java/com/androider/legacy/data/Converter.java
@@ -6,8 +6,6 @@ import java.util.ArrayList;
  * Created by Think on 2015/4/18.
  */
 public interface Converter {
-    public String objectToString(Cachable obj);
-    public String listToString(ArrayList<Cachable> objs);
     public ArrayList<Cachable> stringToList(String str);
     public Cachable stringToObj(String str);
 }
--- app/src/main/java/com/androider/legacy/data/PostConverter.java
+++ app/src/main/java/com/androider/legacy/data/PostConverter.java
@@ -5,6 +5,8 @@ import org.json.JSONObject;
 import org.json.JSONStringer;
 import org.json.JSONTokener;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -12,19 +14,11 @@ import java.util.Date;
  * Created by Think on 2015/4/18.
  */
 public class PostConverter implements Converter{
-    @Override
-    public String objectToString(Cachable obj) {
-        return null;
-    }
-
-    @Override
-    public String listToString(ArrayList<Cachable> objs) {
-        return null;
-    }
 
     @Override
     public ArrayList<Cachable> stringToList(String str) {
-        return null;
+        JSONTokener jsonParser = new JSONTokener(str);
+        while(jsonParser.)
     }
 
     @Override
@@ -36,9 +30,13 @@ public class PostConverter implements Converter{
             String des = jsonPost.getString("des");
             String img = jsonPost.getString("img");
             int seller = jsonPost.getInt("seller");
-            Date publish = Date(jsonPost.getString("publish"));
-            Post post = new Post()
+            Date publish = DateFormat.getInstance().parse(jsonPost.getString("publish"));
+            String abs = jsonPost.getString("abs");
+            Post post = new Post(id,des,img,seller,publish,abs);
+            return post;
         } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
             e.printStackTrace();
         }
         return null;
