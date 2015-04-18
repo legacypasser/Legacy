57e069f1575382d7a514496bfcd74f7cc2ea4661
panboxiaosa <864633261@qq.com>
2015-04-18 18:13:39 +0800

    rebuild

--- app/src/main/java/com/androider/legacy/data/PostConverter.java
+++ app/src/main/java/com/androider/legacy/data/PostConverter.java
@@ -1,10 +1,13 @@
 package com.androider.legacy.data;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONStringer;
 import org.json.JSONTokener;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -24,6 +27,17 @@ public class PostConverter implements Converter{
 
     @Override
     public ArrayList<Cachable> stringToList(String str) {
+        try {
+            ArrayList<Cachable> result = new ArrayList<Cachable>();
+            JSONArray jsonPosts = new JSONArray(str);
+            for(int i = 0;i < jsonPosts.length(); i++){
+                Post item = (Post)stringToObj(jsonPosts.getString(i));
+                result.add(item);
+            }
+            return result;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
@@ -36,9 +50,12 @@ public class PostConverter implements Converter{
             String des = jsonPost.getString("des");
             String img = jsonPost.getString("img");
             int seller = jsonPost.getInt("seller");
-            Date publish = Date(jsonPost.getString("publish"));
-            Post post = new Post()
+            Date publish = DateFormat.getInstance().parse(jsonPost.getString("publish"));
+            String abs = jsonPost.getString("abs");
+            Post post = new Post(id, des, img, seller, publish, abs);
         } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
             e.printStackTrace();
         }
         return null;
